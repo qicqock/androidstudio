49192e8682bd901337d60f3b5ec3be5619416834
qicqock <qicqockqvgiq@gmail.com>
2020-05-24 01:20:02 +0900

    update1

--- WeatherForecastApp/app/src/main/java/com/example/weatherforecastapp/DetailActivity.java
+++ WeatherForecastApp/app/src/main/java/com/example/weatherforecastapp/DetailActivity.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -24,16 +25,54 @@ public class DetailActivity extends AppCompatActivity {
 
         @Override
         protected String doInBackground(String... address) {
+            HttpURLConnection connection = null;
+            InputStreamReader isr = null;
+            BufferedReader reader = null;
+            String JSON = null;
 
             try {
                 URL url = new URL(address[0]);
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection = (HttpURLConnection) url.openConnection();
                 connection.connect();
 
                 InputStream is = connection.getInputStream();
-                InputStreamReader isr = new InputStreamReader(is);
+                isr = new InputStreamReader(is);
 
-                int data = isr.read();
+
+                StringBuffer buffer = new StringBuffer();
+                if (is == null) {
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(is));
+
+                if (buffer.length() == 0) {
+                    return null;
+                }
+
+                JSON = buffer.toString();
+            } catch (IOException e) {
+                Log.e("PlaceholderFragment", "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("PlaceholderFragment", "Error closing stream", e);
+                    }
+                }
+            }
+
+
+
+
+
+                /*int data = isr.read();
                 String content = "";
                 char ch;
                 while (data != -1){
@@ -47,11 +86,12 @@ public class DetailActivity extends AppCompatActivity {
                 e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
-            }
+            }*/
 
             return null;
         }
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -63,8 +103,6 @@ public class DetailActivity extends AppCompatActivity {
             Log.i("contentData",content);
             JSONObject jsonObject = new JSONObject(content);
             String weatherData = jsonObject.getString("weather");
-            Log.i("weatherData",weatherData);
-            JSONArray array = new JSONArray(weatherData);
 
 
         } catch (Exception e) {
--- WeatherForecastApp/app/src/main/java/com/example/weatherforecastapp/MainActivity.java
+++ WeatherForecastApp/app/src/main/java/com/example/weatherforecastapp/MainActivity.java
@@ -1,21 +1,41 @@
 package com.example.weatherforecastapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+
+import android.content.Intent;
 import android.view.Menu;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_refresh,menu);
         return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.action_refresh) {
+            startActivity(new Intent(this, SettingsActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.ForecastFragment);
     }
+
+
+
 }
